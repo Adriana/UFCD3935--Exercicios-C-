66fe76a241fd3e75a6d2bc091d496003dc997d94
adria <adria@DESKTOP-AASC37Q>
2022-04-21 10:05:14 +0100

    Add project files.

---
+++ ConsoleArea/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArea
+{
+    class Retangulo
+    {
+        public Retangulo()
+        {
+        }
+        public int altura, area, comprimento;
+
+        public int Calculo()
+        {
+            area = comprimento * altura;
+            return area;
+        }
+    }
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.Write("Comprimento : ");
+            retangulo.comprimento = int.Parse(Console.ReadLine());
+            Console.Write("Altura :");
+            retangulo.altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Area : " + retangulo.Calculo());
+            Console.Read();
+        }
+    }
+}
---
+++ Fatorial/Fatorial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Fatorial
+    {
+        public long num, fatorial;
+        public Fatorial(long numEntrada)
+        {
+            num = numEntrada;
+            fatorial = 1;
+        }
+        public double Calculo()
+        {
+            while (num > 1)
+            {
+                fatorial= fatorial * num;
+                num= num - 1;
+            }
+            return fatorial;
+        }
+    }
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Número : ");
+            Fatorial fatorial = new Fatorial(long.Parse(Console.ReadLine()));
+            Console.WriteLine("Fatorial : " + fatorial.Calculo());
+            Console.Read();
+
+        }
+    }
+}
+
+        
